d42ae0749f6102ff061f6ce908e85c13f024b605
Liam Murphy <liampm32@gmail.com>
2024-05-06 22:22:51 +1000

    pyjson5: clean up, add tests

--- pkgs/pyjson5.nix
+++ pkgs/pyjson5.nix
@@ -3,23 +3,31 @@
   buildPythonPackage,
   fetchPypi,
   setuptools,
+  wheel,
+  colorama,
   cython,
 }:
 buildPythonPackage rec {
   pname = "pyjson5";
   version = "1.6.6";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-IN+MXbvg1lP0A9qIt1IL5E/I10aXu90erWiEWLVpGgI=";
   };
 
-  pyproject = true;
-  # For some reason it doesn't work if I use `build-system` here...
-  nativeBuildInputs = [
+  build-system = [
     setuptools
+    wheel
     cython
   ];
+
+  nativeCheckInputs = [ colorama ];
+  checkPhase = ''
+    python scripts/run-minefield-test.py
+    python scripts/run-tests.py
+  '';
 
   meta = {
     description = "A JSON5 serializer and parser library for Python 3 written in Cython";
