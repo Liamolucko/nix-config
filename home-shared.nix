e85fcf064d58980696359d51d9199f91e90fa943
Liam Murphy <liampm32@gmail.com>
2024-08-13 17:39:55 +1000

    Manage Zed config via. Nix

--- home-shared.nix
+++ home-shared.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   home.packages = [
     pkgs.binaryen
@@ -41,6 +41,89 @@
 
   home.sessionVariables = {
     EDITOR = "zed --wait";
+  };
+
+  xdg.configFile."zed/settings.json".text = builtins.toJSON {
+    theme = {
+      mode = "system";
+      light = "One Light";
+      dark = "Xcode High Contrast Dark";
+    };
+    buffer_font_size = 14;
+    features = {
+      inline_completion_provider = "none";
+    };
+    assistant = {
+      version = "2";
+      enabled = false;
+    };
+    terminal = {
+      dock = "bottom";
+      alternate_scroll = "on";
+      detect_venv = "off";
+    };
+    auto_install_extensions = {
+      "fish" = true;
+      "git-firefly" = true;
+      "java-eclipse-jdtls" = true;
+      "make" = true;
+      "nix" = true;
+      "ruff" = true;
+      "scheme" = true;
+      "toml" = true;
+      "typst" = true;
+      "verilog" = true;
+      "wgsl" = true;
+      "wit" = true;
+      "xcode-themes" = true;
+    };
+    languages = {
+      "Git Commit" = {
+        wrap_guides = [ 72 ];
+      };
+      "Java" = {
+        formatter = "language_server";
+      };
+      "Markdown" = {
+        format_on_save = "on";
+        soft_wrap = "editor_width";
+      };
+      "Nix" = {
+        tab_size = 2;
+        formatter = {
+          external = {
+            command = "nixfmt";
+            arguments = [ ];
+          };
+        };
+      };
+      "Python" = {
+        formatter = {
+          language_server = {
+            name = "ruff";
+          };
+        };
+      };
+      "Typst" = {
+        soft_wrap = "editor_width";
+      };
+      "Verilog" = {
+        tab_size = 2;
+      };
+    };
+    "lsp" = {
+      "rust-analyzer" = {
+        initialization_options = {
+          checkOnSave.command = "clippy";
+          imports.granularity.group = "module";
+        };
+      };
+      "tinymist" = {
+        initialization_options = {
+          formatterMode = "typstyle";
+        };
+      };
+    };
   };
 
   programs.vscode = {
