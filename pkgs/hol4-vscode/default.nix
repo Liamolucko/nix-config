5d11fa3d5166bdf0c8eb84c58229bc1581376dc0
Liam Murphy <liampm32@gmail.com>
2026-07-26 22:01:37 +1000

    oops

--- pkgs/hol4-vscode/default.nix
+++ pkgs/hol4-vscode/default.nix
@@ -1,13 +1,12 @@
-{ vscode-utils, buildNpmPackage, fetchFromGitHub }:
+{
+  vscode-utils,
+  buildNpmPackage,
+  fetchFromGitHub,
+  vsce,
+}:
 let
   pname = "hol4-vscode";
   version = "0.0.20-unstable-2026-03-27";
-  src = fetchFromGitHub {
-    owner = "HOL-Theorem-Prover";
-    repo = pname;
-    rev = "0a8bfceb8e068714a59f91cf23c754182c774d67";
-    hash = "";
-  };
 
   vsix = buildNpmPackage {
     name = "${pname}-${version}.vsix";
@@ -15,11 +14,15 @@ let
       owner = "HOL-Theorem-Prover";
       repo = pname;
       rev = "0a8bfceb8e068714a59f91cf23c754182c774d67";
-      hash = "";
+      hash = "sha256-SlReEuHTfoXwq4oeilLksu0vhRD7XG7mR+eOebW19xE=";
     };
-    npmDepsHash = "";
+    npmDepsHash = "sha256-YUtdts+BeNdg/whCMxf5KglMHlBxkmZjq+u4eiF3ZTo=";
+
+    nativeBuildInputs = [ vsce ];
+
+    dontNpmBuild = true;
     installPhase = ''
-      npm exec --package=@vscode/vsce -- vsce package --out $out
+      vsce package --out $out --skip-license
     '';
   };
 in
